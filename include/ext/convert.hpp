9fcdc10c836bb6ddd726b1fd691374a2e996f48f
chuikingshek <git@chuikingshek.com>
2025-03-31 11:32:20 +0800

    remove ui8, ui16, ui32 integer-suffix because they are specific to the MSVC compiler

--- include/ext/convert.hpp
+++ include/ext/convert.hpp
@@ -27,41 +27,43 @@ namespace PAINFUL_CPP_STRING_CONVERSION_NAMESPACE {
     static_assert(sizeof(char) == sizeof(char8_t));
 
     namespace details {
-        [[nodiscard]] inline bool is_utf8_1(char32_t const c) noexcept { return c <= 0x7fui32; }
-        [[nodiscard]] inline bool is_utf8_2(char32_t const c) noexcept { return c > 0x7fui32 && c <= 0x7ffui32; }
-        [[nodiscard]] inline bool is_utf8_3(char32_t const c) noexcept { return c > 0x7ffui32 && c <= 0xffffui32; }
-        [[nodiscard]] inline bool is_utf8_4(char32_t const c) noexcept { return c > 0xffffui32 && c <= 0x1f'ffffui32; }
-        [[nodiscard]] inline bool is_utf8_5(char32_t const c) noexcept { return c > 0x1f'ffffui32 && c <= 0x3ff'ffffui32; }
-        [[nodiscard]] inline bool is_utf8_6(char32_t const c) noexcept { return c > 0x3ff'ffffui32 && c <= 0x7fff'ffffui32; }
+        [[nodiscard]] constexpr uint8_t operator""_u8(unsigned long long int const v) noexcept { return static_cast<uint8_t>(v); }
 
-        [[nodiscard]] inline char8_t e_utf8_1(char32_t const c) noexcept { return c & 0x7fui8; }
-        [[nodiscard]] inline char8_t e_utf8_2(char32_t const c) noexcept { return 0b11000000ui8 | (((c >> (6 * 1)) & 0b00011111ui8)); }
-        [[nodiscard]] inline char8_t e_utf8_3(char32_t const c) noexcept { return 0b11100000ui8 | (((c >> (6 * 2)) & 0b00001111ui8)); }
-        [[nodiscard]] inline char8_t e_utf8_4(char32_t const c) noexcept { return 0b11110000ui8 | (((c >> (6 * 3)) & 0b00000111ui8)); }
-        [[nodiscard]] inline char8_t e_utf8_5(char32_t const c) noexcept { return 0b11111000ui8 | (((c >> (6 * 4)) & 0b00000011ui8)); }
-        [[nodiscard]] inline char8_t e_utf8_6(char32_t const c) noexcept { return 0b11111100ui8 | (((c >> (6 * 5)) & 0b00000001ui8)); }
-        [[nodiscard]] inline char8_t e_utf8_m(char32_t const c, int const unit) noexcept { return 0x80ui8 | ((c >> (6 * unit)) & 0x3fui8); }
+        [[nodiscard]] inline bool is_utf8_1(char32_t const c) noexcept { return c <= 0x7fu; }
+        [[nodiscard]] inline bool is_utf8_2(char32_t const c) noexcept { return c > 0x7fu && c <= 0x7ffu; }
+        [[nodiscard]] inline bool is_utf8_3(char32_t const c) noexcept { return c > 0x7ffu && c <= 0xffffu; }
+        [[nodiscard]] inline bool is_utf8_4(char32_t const c) noexcept { return c > 0xffffu && c <= 0x1f'ffffu; }
+        [[nodiscard]] inline bool is_utf8_5(char32_t const c) noexcept { return c > 0x1f'ffffu && c <= 0x3ff'ffffu; }
+        [[nodiscard]] inline bool is_utf8_6(char32_t const c) noexcept { return c > 0x3ff'ffffu && c <= 0x7fff'ffffu; }
 
-        [[nodiscard]] inline bool is_utf16_h(char32_t const c) noexcept { return c >= 0xd800ui32 && c <= 0xdbffui32; }
-        [[nodiscard]] inline bool is_utf16_l(char32_t const c) noexcept { return c >= 0xdc00ui32 && c <= 0xdfffui32; }
+        [[nodiscard]] inline char8_t e_utf8_1(char32_t const c) noexcept { return c & 0b0111'1111_u8; }
+        [[nodiscard]] inline char8_t e_utf8_2(char32_t const c) noexcept { return 0b1100'0000_u8 | (((c >> (6 * 1)) & 0b0001'1111_u8)); }
+        [[nodiscard]] inline char8_t e_utf8_3(char32_t const c) noexcept { return 0b1110'0000_u8 | (((c >> (6 * 2)) & 0b0000'1111_u8)); }
+        [[nodiscard]] inline char8_t e_utf8_4(char32_t const c) noexcept { return 0b1111'0000_u8 | (((c >> (6 * 3)) & 0b0000'0111_u8)); }
+        [[nodiscard]] inline char8_t e_utf8_5(char32_t const c) noexcept { return 0b1111'1000_u8 | (((c >> (6 * 4)) & 0b0000'0011_u8)); }
+        [[nodiscard]] inline char8_t e_utf8_6(char32_t const c) noexcept { return 0b1111'1100_u8 | (((c >> (6 * 5)) & 0b0000'0001_u8)); }
+        [[nodiscard]] inline char8_t e_utf8_m(char32_t const c, int const unit) noexcept { return 0b1000'0000_u8 | ((c >> (6 * unit)) & 0b0011'1111_u8); }
+
+        [[nodiscard]] inline bool is_utf16_h(char32_t const c) noexcept { return c >= 0xd800u && c <= 0xdbffu; }
+        [[nodiscard]] inline bool is_utf16_l(char32_t const c) noexcept { return c >= 0xdc00u && c <= 0xdfffu; }
 
         [[nodiscard]] inline size_t utf32_to_utf8(char32_t const c, char8_t (&s)[8], bool const extended = false) noexcept {
             if (is_utf8_1(c)) {
                 s[0] = e_utf8_1(c);
-                s[1] = 0;
+                s[1] = {};
                 return 1;
             }
             if (is_utf8_2(c)) {
                 s[0] = e_utf8_2(c);
                 s[1] = e_utf8_m(c, 0);
-                s[2] = 0;
+                s[2] = {};
                 return 2;
             }
             if (!is_utf16_h(c) && !is_utf16_l(c) && is_utf8_3(c)) {
                 s[0] = e_utf8_3(c);
                 s[1] = e_utf8_m(c, 1);
                 s[2] = e_utf8_m(c, 0);
-                s[3] = 0;
+                s[3] = {};
                 return 3;
             }
             if (is_utf8_4(c)) {
@@ -69,7 +71,7 @@ namespace PAINFUL_CPP_STRING_CONVERSION_NAMESPACE {
                 s[1] = e_utf8_m(c, 2);
                 s[2] = e_utf8_m(c, 1);
                 s[3] = e_utf8_m(c, 0);
-                s[4] = 0;
+                s[4] = {};
                 return 4;
             }
             if (extended && is_utf8_5(c)) {
@@ -78,7 +80,7 @@ namespace PAINFUL_CPP_STRING_CONVERSION_NAMESPACE {
                 s[2] = e_utf8_m(c, 2);
                 s[3] = e_utf8_m(c, 1);
                 s[4] = e_utf8_m(c, 0);
-                s[5] = 0;
+                s[5] = {};
                 return 5;
             }
             if (extended && is_utf8_6(c)) {
@@ -88,28 +90,28 @@ namespace PAINFUL_CPP_STRING_CONVERSION_NAMESPACE {
                 s[3] = e_utf8_m(c, 2);
                 s[4] = e_utf8_m(c, 1);
                 s[5] = e_utf8_m(c, 0);
-                s[6] = 0;
+                s[6] = {};
                 return 6;
             }
             s[0] = u8'?';
-            s[1] = 0;
+            s[1] = {};
             return 1;
         }
 
-        [[nodiscard]] inline bool is_utf8_1(char8_t const c) noexcept { return (c & 0x80ui8) == 0x00ui8; }
-        [[nodiscard]] inline bool is_utf8_2(char8_t const c) noexcept { return (c & 0xe0ui8) == 0xc0ui8; }
-        [[nodiscard]] inline bool is_utf8_3(char8_t const c) noexcept { return (c & 0xf0ui8) == 0xe0ui8; }
-        [[nodiscard]] inline bool is_utf8_4(char8_t const c) noexcept { return (c & 0xf8ui8) == 0xf0ui8; }
-        [[nodiscard]] inline bool is_utf8_5(char8_t const c) noexcept { return (c & 0xfcui8) == 0xf8ui8; }
-        [[nodiscard]] inline bool is_utf8_6(char8_t const c) noexcept { return (c & 0xfeui8) == 0xfcui8; }
-        [[nodiscard]] inline bool is_utf8_m(char8_t const c) noexcept { return (c & 0xc0ui8) == 0x80ui8; }
+        [[nodiscard]] inline bool is_utf8_1(char8_t const c) noexcept { return (c & 0x80_u8) == 0x00_u8; }
+        [[nodiscard]] inline bool is_utf8_2(char8_t const c) noexcept { return (c & 0xe0_u8) == 0xc0_u8; }
+        [[nodiscard]] inline bool is_utf8_3(char8_t const c) noexcept { return (c & 0xf0_u8) == 0xe0_u8; }
+        [[nodiscard]] inline bool is_utf8_4(char8_t const c) noexcept { return (c & 0xf8_u8) == 0xf0_u8; }
+        [[nodiscard]] inline bool is_utf8_5(char8_t const c) noexcept { return (c & 0xfc_u8) == 0xf8_u8; }
+        [[nodiscard]] inline bool is_utf8_6(char8_t const c) noexcept { return (c & 0xfe_u8) == 0xfc_u8; }
+        [[nodiscard]] inline bool is_utf8_m(char8_t const c) noexcept { return (c & 0xc0_u8) == 0x80_u8; }
 
-        [[nodiscard]] inline char32_t d_utf8_2(char8_t const c) noexcept { return (c & 0x1fui32) << (6 * 1); }
-        [[nodiscard]] inline char32_t d_utf8_3(char8_t const c) noexcept { return (c & 0x0fui32) << (6 * 2); }
-        [[nodiscard]] inline char32_t d_utf8_4(char8_t const c) noexcept { return (c & 0x07ui32) << (6 * 3); }
-        [[nodiscard]] inline char32_t d_utf8_5(char8_t const c) noexcept { return (c & 0x03ui32) << (6 * 4); }
-        [[nodiscard]] inline char32_t d_utf8_6(char8_t const c) noexcept { return (c & 0x01ui32) << (6 * 5); }
-        [[nodiscard]] inline char32_t d_utf8_m(char8_t const c, int const unit) noexcept { return (c & 0x3fui32) << (6 * unit); }
+        [[nodiscard]] inline char32_t d_utf8_2(char8_t const c) noexcept { return (c & 0x1fu) << (6 * 1); }
+        [[nodiscard]] inline char32_t d_utf8_3(char8_t const c) noexcept { return (c & 0x0fu) << (6 * 2); }
+        [[nodiscard]] inline char32_t d_utf8_4(char8_t const c) noexcept { return (c & 0x07u) << (6 * 3); }
+        [[nodiscard]] inline char32_t d_utf8_5(char8_t const c) noexcept { return (c & 0x03u) << (6 * 4); }
+        [[nodiscard]] inline char32_t d_utf8_6(char8_t const c) noexcept { return (c & 0x01u) << (6 * 5); }
+        [[nodiscard]] inline char32_t d_utf8_m(char8_t const c, int const unit) noexcept { return (c & 0x3fu) << (6 * unit); }
 
         [[nodiscard]] inline size_t utf8_to_utf32(char8_t const* const s, size_t const n, char32_t& c, bool const extended = false) noexcept {
             if (is_utf8_1(s[0])) {
@@ -150,11 +152,11 @@ namespace PAINFUL_CPP_STRING_CONVERSION_NAMESPACE {
             return 1;
         }
 
-        [[nodiscard]] inline bool is_utf16_h(char16_t const c) noexcept { return (c & 0xfc00ui16) == 0xd800ui16; }
-        [[nodiscard]] inline bool is_utf16_l(char16_t const c) noexcept { return (c & 0xfc00ui16) == 0xdc00ui16; }
+        [[nodiscard]] inline bool is_utf16_h(char16_t const c) noexcept { return (c & 0xfc00u) == 0xd800u; }
+        [[nodiscard]] inline bool is_utf16_l(char16_t const c) noexcept { return (c & 0xfc00u) == 0xdc00u; }
 
         [[nodiscard]] inline char32_t utf16_to_utf32(char16_t const h, char16_t const l) noexcept {
-            return 0x10000ui32 + ((h & 0x3ffui32) << 10) | (l & 0x3ffui32);
+            return 0x10000u + ((h & 0x3ffu) << 10) | (l & 0x3ffu);
         }
 
         [[nodiscard]] inline size_t utf16_to_utf32(char16_t const* const s, size_t const n, char32_t& c) {
@@ -171,16 +173,16 @@ namespace PAINFUL_CPP_STRING_CONVERSION_NAMESPACE {
         }
 
         [[nodiscard]] inline size_t utf32_to_utf16(char32_t const c, char16_t (&s)[4]) noexcept {
-            if (!is_utf16_h(c) && !is_utf16_l(c) && c <= 0xffffui32) {
-                s[0] = c & 0xffffui16;
-                s[1] = 0;
+            if (!is_utf16_h(c) && !is_utf16_l(c) && c <= 0xffffu) {
+                s[0] = c & 0xffffu;
+                s[1] = {};
                 return 1;
             }
-            if (c > 0xffffui32 && c <= 0x10'ffffui32) {
-                char32_t const d = c - 0x1'0000ui32;
-                s[0] = 0xd800ui16 | ((d >> 10) & 0x3ffui16);
-                s[1] = 0xdc00ui16 | (d & 0x3ffui16);
-                s[2] = 0;
+            if (c > 0xffffu && c <= 0x10'ffffu) {
+                char32_t const d = c - 0x1'0000u;
+                s[0] = 0xd800u | ((d >> 10) & 0x3ffu);
+                s[1] = 0xdc00u | (d & 0x3ffu);
+                s[2] = {};
                 return 2;
             }
             s[0] = u'?';
